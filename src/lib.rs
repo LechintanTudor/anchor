06f40cbc96e3559def33ec5873c083dead7c7d3c
Tudor Lechintan <lechintantudor02@gmail.com>
2022-07-11 14:36:27 +0300

    Update smallvec and reexport glam, wgpu and winit

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 pub(crate) mod utils;
 
+use crate::core::{Config, GameBuilder, GameResult};
+
 pub mod core;
 pub mod graphics;
 pub mod input;
 
-use crate::core::{Config, GameBuilder, GameResult};
+pub use glam;
+pub use winit;
+pub use wgpu;
 
 pub fn run<G>(config: Config, game_builder: G) -> GameResult<()>
 where
